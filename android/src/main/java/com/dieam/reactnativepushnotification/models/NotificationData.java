133ee4955ae5cd087cdc68cd23677c2bf7fa6759
Adrián Scott <32658258+AdrianLScott@users.noreply.github.com>
2022-06-09 13:59:05 -0600

    add notification to EarthquakeAlert model

---
+++ android/src/main/java/com/dieam/reactnativepushnotification/models/NotificationData.java
@@ -0,0 +1,16 @@
+package com.dieam.reactnativepushnotification.models;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+public class NotificationData {
+    public String title;
+    public String message;
+    public String soundName;
+
+    public NotificationData(JSONObject notification) throws JSONException {
+        this.title = notification.getString("title");
+        this.message = notification.getString("message");
+        this.soundName = notification.getString("soundName");
+    }
+}
