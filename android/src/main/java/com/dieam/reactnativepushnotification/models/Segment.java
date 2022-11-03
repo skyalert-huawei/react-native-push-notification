26ff914adba4283788ac78ceef70de4886e0e583
Adrián Scott <32658258+AdrianLScott@users.noreply.github.com>
2022-11-03 08:14:19 -0700

    Add name attribute to Segment model

--- android/src/main/java/com/dieam/reactnativepushnotification/models/Segment.java
+++ android/src/main/java/com/dieam/reactnativepushnotification/models/Segment.java
@@ -7,11 +7,12 @@ import org.json.JSONObject;
 public class Segment {
     public String id;
     public String tag;
-
+    public String name;
 
     public Segment(JSONObject segment) throws JSONException {
         this.id = segment.getString("id");
         this.tag = segment.getString("tag");
+        this.name = segment.getString("name");
     }
 
 }
